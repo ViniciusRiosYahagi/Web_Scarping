d1acc70a486055c1cd06b6cebbd472cf2380222a
Vinicius Yahagi Rios <viniciusyahagirios@hotmail.com>
2025-04-28 20:24:07 -0300

    File System
    Adding fs to generate a json file with data of scraping.

--- books.js
+++ books.js
@@ -1,51 +1,47 @@
 import puppeteer from "puppeteer";
+import fs from "fs";
 
 (async () => {
-    const browser = await puppeteer.launch({ headless: false })
-    const page = await browser.newPage()
-    await page.goto('https://books.toscrape.com/')
-    await page.waitForNetworkIdle()
+  const browser = await puppeteer.launch({ headless: false });
+  const page = await browser.newPage();
+  await page.goto("https://books.toscrape.com/");
+  await page.waitForNetworkIdle();
 
-    const result = await page.evaluate( async () => {
-        await new Promise(resolve => {
-            const distance = 1000
-            let scrolledAmount = 0
+  const result = await page.evaluate(async () => {
+    await new Promise((resolve) => {
+      const distance = 1000;
+      let scrolledAmount = 0;
 
-            const interval = setInterval(() => {
-                window.scrollBy(0, distance)
-                scrolledAmount += distance
+      const interval = setInterval(() => {
+        window.scrollBy(0, distance);
+        scrolledAmount += distance;
 
-                
-                if (scrolledAmount >= document.body.scrollHeight) {
-                    clearInterval(interval)
-                    resolve()
-                }
-
-            }, 100)
-        })
-
-
-
-        const books = []
-        const booksQuery = document.querySelectorAll("h3 > a")
-        const pricesQuery = document.querySelectorAll(".price_color")
-        const availbeQuery = document.querySelectorAll(".instock")
-        
-        for (let i = 0; i < booksQuery.length; i++) {
-            books.push({
-                src: booksQuery[i].href,
-                title: booksQuery[i].title,
-                price: pricesQuery[i].textContent,
-                status: availbeQuery[i]?.textContent.trim()
-            })
+        if (scrolledAmount >= document.body.scrollHeight) {
+          clearInterval(interval);
+          resolve();
         }
-        
+      }, 100);
+    });
 
-        return books
-    })
+    const books = [];
+    const booksQuery = document.querySelectorAll("h3 > a");
+    const pricesQuery = document.querySelectorAll(".price_color");
+    const availbeQuery = document.querySelectorAll(".instock");
 
-    
-    console.log(result)
+    for (let i = 0; i < booksQuery.length; i++) {
+      books.push({
+        src: booksQuery[i].href,
+        title: booksQuery[i].title,
+        price: pricesQuery[i].textContent,
+        status: availbeQuery[i]?.textContent.trim(),
+      });
+    }
 
-    browser.close()
-})()
+    return books;
+  });
+
+  fs.writeFileSync("book.json", JSON.stringify(result, null, 2));
+  console.log(result);
+
+  browser.close();
+})();
